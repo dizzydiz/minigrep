21437c1d196fe1796bdf136459c09580a4c654c5
stas <stas.ubuntu@gmail.com>
2023-05-21 00:07:53 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,39 @@ fn main() {
     let args:Vec<String> = env::args().collect();
     // dbg!(args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
+    
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
     .expect("Should have been able to read the file");
 
     println!("With text:\n {contents}");
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let file_path = args[2].clone();    
+
+        // Config {
+        //     query, file_path        
+        // }
+        Config { query: query, file_path: file_path }
+    }
+}
+
+            
+// fn parse_config(args: &Vec<String>) -> (&str, &str) {
+//     let query = &args[1];
+//     let file_path = &args[2];
+    
+//     (query, file_path)
+// }
