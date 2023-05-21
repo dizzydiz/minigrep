def2d95b8a63f0d0807e4d896c40bdbbbc326498
Stas Badaev <stas@cylus.com>
2023-05-21 17:31:50 +0300

    move-to-lib

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
+use minigrep::Config;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
@@ -15,34 +15,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-    .expect("Should have been able to read the file");
-
-    println!("With text:\n {contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'ststic str> {
-        if args.len() != 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        
-        Ok(Config{query, file_path})
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
-
-            
-// fn parse_config(args: &Vec<String>) -> (&str, &str) {
-//     let query = &args[1];
-//     let file_path = &args[2];
-    
-//     (query, file_path)
-// }
